0b300ad207f3a757898c4f5bf5b09c30eb80173f
distilledx <alissxlace@proton.me>
2023-09-18 15:41:28 +0530

    Added color splash

---
+++ colorsp.c
@@ -0,0 +1,30 @@
+//   ____      _            ____        _           _
+//  / ___|___ | | ___  _ __/ ___| _ __ | | __ _ ___| |__
+// | |   / _ \| |/ _ \| '__\___ \| '_ \| |/ _` / __| '_ \ 
+// | |__| (_) | | (_) | |   ___) | |_) | | (_| \__ \ | | |
+//  \____\___/|_|\___/|_|  |____/| .__/|_|\__,_|___/_| |_|
+//                               |_|
+//
+// Display a full screen of a single color
+
+#include "./src/main.h"
+#include <unistd.h>
+
+int main()
+{
+    struct FramebufferInfo fbi;
+    initfb(&fbi);
+
+    int x, y;
+
+    for (x = 0; x < fbi.vinfo.xres; x++)
+        for (y = 0; y < fbi.vinfo.yres; y++)
+        {
+            render(x, y, pixel_color(0xFF, 0x00, 0xFF, &fbi.vinfo), &fbi.vinfo, &fbi.finfo, fbi.fbp);
+        }
+
+    sleep(1);
+    cleanfb(&fbi);
+
+    return 0;
+}
