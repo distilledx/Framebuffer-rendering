61acd52e80bb1883c0dd8625df0bf4a386ad38df
distilledx <alissxlace@proton.me>
2023-09-22 00:48:39 +0530

    Included gif in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 mkdir build
 gcc ./src/colorsp.c ./src/main.c -o ./build/colorsplash
 gcc ./src/imagesp.c ./helpers/lodepng.c ./src/main.c -o ./build/imagesplash
+gcc ./src/gifsp.c ./src/main.c  -o ./build/gifsplash -lgif
 
 if [ -x "./build/colorsplash" ]; then
     chmod +x ./build/colorsplash
@@ -8,4 +9,8 @@ fi
 
 if [ -x "./build/imagesplash" ]; then
     chmod +x ./build/imagesplash
+fi
+
+if [ -x "./build/gifsplash" ]; then
+    chmod +x ./build/gifsplash
 fi
